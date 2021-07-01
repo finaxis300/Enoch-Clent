1c3d952ff880489a24d84f773e759b0af7f6708f
rupeshjha1 <jharupesh223@gmail.com>
2021-07-01 19:15:24 +0530

    react + typescript

--- Enoch dApp/react-typescript/pages/index.tsx
+++ Enoch dApp/react-typescript/pages/index.tsx
@@ -6,24 +6,13 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import type { AppProps } from 'next/app'
 
-<<<<<<< HEAD
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+
 }
 
 export default MyApp
 
-=======
-export default function Home() {
-  return (
-   
-		<div className="container-fluid">
-			{/*  */}
-		</div>
-
-  )
-}
->>>>>>> c9825f93623d7fe4bb9aa6518292ef6362876d81
