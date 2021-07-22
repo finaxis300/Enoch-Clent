cf2442c5722dc05e13b34a9f46b8b0dddbe1040c
Hamza Yousaf <Hamzayousaf336@gmail.com>
2021-07-22 09:00:35 +0500

    proper selfle done

--- Enoch dApp/react-typescript/pages/kyc/doc/proper-selfie.tsx
+++ Enoch dApp/react-typescript/pages/kyc/doc/proper-selfie.tsx
@@ -1,102 +1,17 @@
 import React, { useState } from 'react'
-import Header from '../../component/header'
+import Header from '../../../Component/header'
 import Link from 'next/link'
+import SubHeaderDoc from '../../../Component/kyc/doc/sub-header'
+import BredCrumbDoc from '../../../Component/kyc/doc/bredcrumb'
 
 const ProperSelfie: React.FC = () => {
   return (
     <div className='container-fluid dashboard-body-bg'>
       <Header />
-      <div className='row'>
-        <div className='dashboard-header-bredcrumb'>
-          <div className='ezl-dashboard-container'>
-            <div className='col-12'>
-              <ul>
-                <li className='mr-2'>
-                 '}
-                </li>
-                <li className='mr-1'>
-                  <a href='#' className='dsh-active-black'>
-                    {' '}
-                  </a>
-                </li>
-                <li>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      <BredCrumbDoc />
 
       <div className='KYC-BODY ezl-dashboard-container'>
-        <div className='Linking-wallet-four-icons-sect'>
-          <div className='four-icons-list'>
-            <div className='row'>
-              <div className='col-md-3'>
-                <div className='Linking-wallet-icon Linking-wallet-icon-1'>
-                  <div className='Linking-wallet-icon-img Linking-wallet-icon-1-img'>
-                    <img
-                      src='/images/step-2-imgs/icon-1.png'
-                      alt='step-1'
-                      className='img-fluid'
-                    />
-                  </div>
-                  <div className='Linking-wallet-icon-text Linking-wallet-icon-1-text'>
-                   
-                  </div>
-                </div>
-              </div>
-
-              <div className='col-md-3'>
-                <div className='Linking-wallet-icon Linking-wallet-icon-2'>
-                  <div className='Linking-wallet-icon-img Linking-wallet-icon-2-img Linking-wallet-icon-img-act'>
-                    <img
-                      src='/images/step-2-imgs/icon-2-act.png'
-                      alt='step-2'
-                      className='img-fluid'
-                    />
-                  </div>
-                  <div className='Linking-wallet-icon-text Linking-wallet-icon-2-text'>
-                    <h1>Step 2</h1>
-                    <h2>Linking Wallet</h2>
-                  </div>
-                </div>
-              </div>
-
-              <div className='col-md-3'>
-                <div className='Linking-wallet-icon Linking-wallet-icon-3'>
-                  <div className='Linking-wallet-icon-img Linking-wallet-icon-3-img'>
-                    <img
-                      src='/images/step-2-imgs/icon-3-act.png'
-                      alt='step-3'
-                      className='img-fluid'
-                    />
-                  </div>
-                  <div className='Linking-wallet-icon-text Linking-wallet-icon-3-text Linking-wallet-active'>
-                    <h1>Step 3</h1>
-                    <h2>User ID Documentation</h2>
-                  </div>
-                </div>
-              </div>
-
-              <div className='col-md-3'>
-                <div className='Linking-wallet-icon Linking-wallet-icon-4'>
-                  <div className='Linking-wallet-icon-img Linking-wallet-icon-4-img'>
-                    <img
-                      src='/images/step-2-imgs/icon-4.png'
-                      alt='step-4'
-                      className='img-fluid'
-                    />
-                  </div>
-                  <div className='Linking-wallet-icon-text Linking-wallet-icon-4-text'>
-                    <h1>Step 4</h1>
-                    <h2>KYC Validation</h2>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <SubHeaderDoc />
         <div className='Steps-to-a-Proper-Selfie-headings'>
           <h1>Steps to a Proper Selfie</h1>
         </div>
